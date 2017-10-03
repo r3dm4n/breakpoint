2c3580e6aa23df9e93cb7393fae142d18b8173c5
Alexandru Corut <r3dm4n@me.com>
2017-10-03 16:33:41 +0100

    add logout func

--- breakpoint/Controller/MeVC.swift
+++ breakpoint/Controller/MeVC.swift
@@ -24,7 +24,17 @@ class MeVC: UIViewController {
     
     
     @IBAction func signOutBtnPressed(_ sender: Any) {
-        try! Auth.auth().signOut()
-        dismiss(animated: true, completion: nil)
+        let logoutPopup = UIAlertController(title: "Logout?", message: "Are you sure you want to logout?", preferredStyle: .actionSheet)
+        let logoutAction = UIAlertAction(title: "Logout?", style: .destructive) { (buttonTapped) in
+            do {
+                try Auth.auth().signOut()
+                let authVC = self.storyboard?.instantiateViewController(withIdentifier: "AuthVC") as? AuthVC
+                self.present(authVC!, animated: true, completion: nil)
+            } catch {
+                print(error)
+            }
+        }
+        logoutPopup.addAction(logoutAction)
+        present(logoutPopup, animated: true, completion: nil)
     }
 }
