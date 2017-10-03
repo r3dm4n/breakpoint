85d46a520569292094ffda615d6c0d2aa02288d9
Alexandru Corut <r3dm4n@me.com>
2017-10-03 13:54:17 +0100

    added FeedCell

---
+++ breakpoint/View/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  breakpoint
+//
+//  Created by r3d on 03/10/2017.
+//  Copyright © 2017 Alexandru Corut. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
