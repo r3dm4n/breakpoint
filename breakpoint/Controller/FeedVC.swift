e75a8cae79d03ec2b61cd789f04e9fd8dea62062
Alexandru Corut <r3dm4n@me.com>
2017-10-03 14:58:09 +0100

    added func to get all feed messages

--- breakpoint/Controller/FeedVC.swift
+++ breakpoint/Controller/FeedVC.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class FeedVC: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    var messageArray = [Message]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DataService.instance.getAllFeedMessages { (returnedMessagesArray) in
+            self.messageArray = returnedMessagesArray
+            self.tableView.reloadData()
+            
+        }
     }
 
-    
+
 }
 
+extension FeedVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messageArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "feedCell") as? FeedCell else { return UITableViewCell() }
+        let image = UIImage.init(named: "defaultProfileImage")
+        let message = messageArray[indexPath.row]
+        
+        cell.configureCell(profileImg: image!, email: message.senderId, content: message.content)
+        return cell
+    }
+}
