fa57859bc3d8716651a9b44fd3ca8bfbe542dcf1
Alexandru Corut <r3dm4n@me.com>
2017-10-03 14:00:08 +0100

    added outlets

--- breakpoint/View/FeedCell.swift
+++ breakpoint/View/FeedCell.swift
@@ -10,15 +10,10 @@ import UIKit
 
 class FeedCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    //Outlets
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
 
 }
