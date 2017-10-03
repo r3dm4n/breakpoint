b2d179f90a8d42a6ddf0eba5f77d80f054ff72ad
Alexandru Corut <r3dm4n@me.com>
2017-10-03 13:18:09 +0100

    bindToKeyboard() extension

--- breakpoint/Controller/CreatePostVC.swift
+++ breakpoint/Controller/CreatePostVC.swift
@@ -22,6 +22,7 @@ class CreatePostVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         textView.delegate = self
+        sendBtn.bindToKeyboard()
         
     }
 
---
+++ breakpoint/Extensions/UIViewExt.swift
@@ -0,0 +1,28 @@
+//
+//  UIViewExt.swift
+//  breakpoint
+//
+//  Created by r3d on 03/10/2017.
+//  Copyright © 2017 Alexandru Corut. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func bindToKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    @objc private func keyboardWillChange(_ notification: NSNotification) {
+        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! UInt
+        let beginningFrame = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let endFrame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let deltyY = endFrame.origin.y - beginningFrame.origin.y
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIViewKeyframeAnimationOptions(rawValue: curve), animations: {
+            self.frame.origin.y += deltyY
+        }, completion: nil)
+    }
+}
