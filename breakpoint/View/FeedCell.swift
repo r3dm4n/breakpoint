e9c3e374382291bc4e0ef6288557b938c0be8f11
Alexandru Corut <r3dm4n@me.com>
2017-10-03 14:57:09 +0100

    added configureCell func

--- breakpoint/View/FeedCell.swift
+++ breakpoint/View/FeedCell.swift
@@ -15,5 +15,10 @@ class FeedCell: UITableViewCell {
     @IBOutlet weak var emailLbl: UILabel!
     @IBOutlet weak var contentLbl: UILabel!
     
+    func configureCell(profileImg: UIImage, email: String, content: String) {
+        self.profileImg.image = profileImg
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
 
 }
